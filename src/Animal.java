b6182f509a962ed0a8d54432de6c6027e25d4f9d
adamd <adam.d.hinton@gmail.com>
2024-02-22 09:55:59 -0500

    flesh out animal class

--- src/Animal.java
+++ src/Animal.java
@@ -9,4 +9,22 @@ public class Animal {
         this.size = size;
         this.weight = weight;
     }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "type='" + type + '\'' +
+                ", size='" + size + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public void move(String speed){
+        System.out.println(type + " moves " + speed);
+    }
+
+    public void makeNoise(){
+        System.out.println(type + " makes some kind of noise");
+    }
+
 }
