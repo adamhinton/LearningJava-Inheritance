e38fa3dd1406f3af31b82a089b2f713f455c6aa8
adamd <adam.d.hinton@gmail.com>
2024-02-22 09:54:32 -0500

    basic animal class

---
+++ src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+
+    private String type;
+    private String size;
+    private double weight;
+
+    public Animal(String type, String size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+}
